092ae8193da1a9abaab1756ac656ddbe9bb68082
Przemysław Fikas <fikasp@gmail.com>
2022-02-17 00:19:37 +0100

    Add folder modules

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListaComponent } from './components/lista/lista.component';
-import { DetaleComponent } from './components/detale/detale.component';
+import { AppComponent } from '../components/app.component';
+import { ListaComponent } from '../components/lista/lista.component';
+import { DetaleComponent } from '../components/detale/detale.component';
 
-import { FilmyService } from './services/filmy.service';
+import { FilmyService } from '../services/filmy.service';
 
 @NgModule({
   declarations: [
