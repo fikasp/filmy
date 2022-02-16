8750422bc8e2916c5f8ce77cbd56aab4a91ac2f7
Przemysław Fikas <fikasp@gmail.com>
2022-02-16 01:31:35 +0100

    Filmy Service

--- src/app/filmy.service.ts
+++ src/app/filmy.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+export interface Film {
+  id: Number 
+  tytul: String
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class FilmyService {
+
+  private filmy: Film[] = [
+    {id: 0, tytul: 'Ojciec chrzestny 1'},
+    {id: 1, tytul: 'Ojciec chrzestny 2'},
+    {id: 2, tytul: 'Ojciec chrzestny 3'}
+  ]
+
+  wszystkieFilmy(): Film[] {
+    return this.filmy;
+  }
 
   constructor() { }
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmyService } from '../filmy.service';
+
+export interface Film {
+  id: Number 
+  tytul: String
+}
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
+
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  filmy: Film[] = [];
 
-  ngOnInit(): void {
+  constructor( private fs: FilmyService) { }
+ 
+  ngOnInit() {
+    this.filmy = this.fs.wszystkieFilmy();
+    console.log(this.filmy);
+    
   }
 
 }
+
+
